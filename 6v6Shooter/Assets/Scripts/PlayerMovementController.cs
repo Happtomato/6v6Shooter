e52c586870a55c176e1d794c3309570ba1f1fb9c
Happtomato <40277552+Happtomato@users.noreply.github.com>
2023-07-08 15:33:21 +0200

    camera movement

--- 6v6Shooter/Assets/Scripts/PlayerMovementController.cs
+++ 6v6Shooter/Assets/Scripts/PlayerMovementController.cs
@@ -1,18 +1,119 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool _readyToJump;
+
+    public float walkSpeed;
+    public float sprintSpeed;
+
+    [Header("Keybindings")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool _grounded;
+
+    public Transform orientation;
+
+    float _horizontalInput;
+    float _verticalInput;
+
+    Vector3 _moveDirection;
+
+    Rigidbody _rb;
+
+    private void Start()
     {
-        
+        MyInput();
+        _rb = GetComponent<Rigidbody>();
+        _rb.freezeRotation = true;
+
+        _readyToJump = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // ground check
+        _grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, whatIsGround);
+
+        SpeedControl();
+
+        // handle drag
+        if (_grounded)
+            _rb.drag = groundDrag;
+        else
+            _rb.drag = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+
+        // when to jump
+        if(Input.GetKey(jumpKey) && _readyToJump && _grounded)
+        {
+            _readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        _moveDirection = orientation.forward * _verticalInput + orientation.right * _horizontalInput;
+
+        // on ground
+        if(_grounded)
+            _rb.AddForce(_moveDirection.normalized * (moveSpeed * 10f), ForceMode.Force);
+
+        // in air
+        else if(!_grounded)
+            _rb.AddForce(_moveDirection.normalized * (moveSpeed * 10f * airMultiplier), ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        var velocity = _rb.velocity;
+        Vector3 flatVel = new Vector3(velocity.x, 0f, velocity.z);
+
+        // limit velocity if needed
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            _rb.velocity = new Vector3(limitedVel.x, _rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        // reset y velocity
+        var velocity = _rb.velocity;
+        velocity = new Vector3(velocity.x, 0f, velocity.z);
+        _rb.velocity = velocity;
+
+        _rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        _readyToJump = true;
     }
 }
